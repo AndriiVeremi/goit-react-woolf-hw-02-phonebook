9c2da644257ee9543c322164fe89615d0259302b
Andrii Veremii <dashuk10@gmail.com>
2024-02-10 20:52:23 +0100

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -19,12 +19,9 @@ export class ContactForm extends Component {
     this.reset();
   };
 
-  reset() {
-    this.setState({
-        name: '',
-        number: '',
-    });
-  }
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
 
   render() {
     return (
@@ -35,6 +32,7 @@ export class ContactForm extends Component {
             <Input
               type="text"
               name="name"
+              value={this.state.name}
               onChange={this.handelChange}
               pattern="^[a-zA-Zа-яА-Я]+(([' ][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -46,6 +44,7 @@ export class ContactForm extends Component {
             <Input
               type="tel"
               name="number"
+              value={this.state.number}
               onChange={this.handelChange}
               pattern="\+?\d{1,4}?[.\s]?\(?\d{1,3}?\)?[.\s]?\d{1,4}[.\s]?\d{1,4}[.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
